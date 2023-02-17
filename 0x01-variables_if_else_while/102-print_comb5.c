4544578f6a40f9d68f47c5d7354e1f2a8439c54c
Thabo <Denzel.sitt@outlook.com>
2023-02-17 14:52:41 -0800

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,8 +17,8 @@ int main(void)
 				putchar(i / 10 + '0');
 				putchar(i % 10 + '0');
 				putchar(' ');
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
 
 				if (i * 100 + j != 9899)
 				{
